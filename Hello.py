95e74c50de5f5db6ac0e5d9c801f64ea6fc5126d
Jéssica Ramos <jessica.luiza297@gmail.com>
2023-06-10 12:23:37 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,9 +4,19 @@ st.set_page_config(
     page_title="Hello"
 )
 
-st.write("# Datavis project")
+st.write("# Demographic Indicators")
 
 st.sidebar.success("Select a page above.")
 
-st.markdown("Add dataset description"
-)
+st.markdown("""
+This dashboard presents data from demographic indicators for countries and regions around the world.
+""")
+
+st.markdown("""
+## Data
+
+The 2022 Revision of World Population Prospects is the twenty-seventh edition of official United Nations population estimates and projections that have been prepared by the Population Division of the Department of Economic and Social Affairs of the United Nations Secretariat.
+It presents population estimates from 1950 to the present for 237 countries or areas, underpinned by analyses of historical demographic trends.
+
+Source: [World Population Prospects 2022](https://population.un.org/wpp/)
+""")
